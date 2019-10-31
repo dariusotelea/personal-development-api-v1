b8d6629ded324749a76042be3606065637a964ed
Darius Otelea <darius.otelea@luminos.software>
2019-10-31 17:38:03 +0200

    Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    render json: user
+  end
+
+  private
+
+  def user_params
+    json_api_params(:first_name, :last_name, :profile_picture)
+  end
+end
